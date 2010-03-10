cea1c449c46f706f521c7d86d7da16da37b0ef01
hitode909 <hitode909@gmail.com>
2010-03-10 18:32:47 +0900

    no search query

--- searcher.rb
+++ searcher.rb
@@ -4,4 +4,14 @@ $:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) ||
   $:.include?(File.expand_path(File.dirname(__FILE__)))
 require 'model'
 
+exit unless ARGV.length > 0
 search(ARGV.first, Dir.pwd)
+
+
+
+
+
+
+
+
+
