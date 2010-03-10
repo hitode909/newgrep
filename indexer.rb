3502ebeb37c45eda9b0bbf993f1cdbe26862b8be
hitode909 <hitode909@gmail.com>
2010-03-10 18:28:40 +0900

    counter

--- indexer.rb
+++ indexer.rb
@@ -4,12 +4,16 @@ $:.unshift(File.dirname(__FILE__)) unless $:.include?(File.dirname(__FILE__)) ||
                                           $:.include?(File.expand_path(File.dirname(__FILE__)))
 require 'model'
 
-ARGV.each {|filename|
+ARGV.each_with_index {|filename, index|
   begin
-    puts "indexing #{filename}"
+    puts "(#{index+1}/#{ARGV.length}) indexing #{filename}"
     document = find_document(filename)
     document.index if document and document.should_index
   rescue => e
     puts e
   end
 }
+
+
+
+
